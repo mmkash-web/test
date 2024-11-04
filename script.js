4088ea8d60201dee0f7ef953f31d12f246047c8c
mmkash-web <manuuduke0@gmail.com>
2024-11-04 16:49:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+function plusSlides(n) {
+    slideIndex += n - 1;
+    showSlides();
+}
+
+function openGallery(galleryId) {
+    document.getElementById(galleryId).style.display = "block";
+}
+
+function closeGallery(galleryId) {
+    document.getElementById(galleryId).style.display = "none";
+}
